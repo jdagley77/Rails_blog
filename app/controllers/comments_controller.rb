00f921580308b8e12df14c3f13be8a0c151d392c
Jack Dagley <jack.dagley77@gmail.com>
2017-02-12 14:21:30 -0800

    add minor syntax adjustment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
-
 	http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
-
+	
 	def create
     	@article = Article.find(params[:article_id])
     	@comment = @article.comments.create(comment_params)
@@ -15,9 +14,9 @@ class CommentsController < ApplicationController
 	  	redirect_to article_path(@article)
 	end
 
- private
-   def comment_params
-      params.require(:comment).permit(:commenter, :body)
-   end
+	private
+		def comment_params
+	    	params.require(:comment).permit(:commenter, :body)
+		end
 
 end
